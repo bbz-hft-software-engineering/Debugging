927809663dc71c69ad9369d81f0ba6c165b38369
swa87 <Stefan.Waskow@bbz-sh.ch>
2023-02-25 11:54:30 +0100

    fertig

--- Debugging/Debugging.cpp
+++ Debugging/Debugging.cpp
@@ -15,11 +15,11 @@ bool is_prime(int n);
 int main()
 {
     
-    aufgabe1();
+    //aufgabe1();
     //aufgabe2();
     //aufgabe3();
     //aufgabe4();
-    //aufgabe5();
+    aufgabe5();
     //aufgabe6();
 
 }
@@ -27,7 +27,7 @@ int main()
 // Das Programm soll Hallo Welt ausgeben. (immer nur ein Buchstabe pro Zeile.)
 void aufgabe1() {
     string str = "Hallo, Welt!";
-    int i = 1;
+    int i = 0;
     int size = str.length();
     while (i <= size) {
         cout << str[i] << endl;
@@ -39,16 +39,23 @@ void aufgabe1() {
 void aufgabe2() {
     int x = 10;
     int y = 0;
-    int result = x / y;
-
-    std::cout << "Das Ergebnis ist: " << result << std::endl;
+    
+    if (y == 0)
+    {
+        cout << "division trough 0 not possible you little shit";
+    }
+    else
+    {
+        int result = x / y;
+        std::cout << "Das Ergebnis ist: " << result << std::endl;
+    }
 }
 
 // Das Programm soll die Zahlen von 1 bis 5 ausgeben.
 void aufgabe3() {
     int array[5] = { 1, 2, 3, 4, 5 };
 
-    for (int i = 0; i <= 5; i++) {
+    for (int i = 0; i < 5; i++) {
         std::cout << array[i] << std::endl;
     }
 }
@@ -56,7 +63,7 @@ void aufgabe3() {
 
 // Das Programm soll die Zahlen von 0 bis 9 ausgeben und dann die Zahlen von 9 bis 0.
 void aufgabe4() {
-    int num = 10;
+    int num = 9;
     int i;
 
     for (i = 0; i < num; i++) {
@@ -70,7 +77,7 @@ void aufgabe4() {
 
 }
 
-// Das Programm soll dopplete Buchstaben in Wörtern löschen.
+// Das Programm soll doppelte Buchstaben in Wörtern löschen.
 void aufgabe5() {
     string my_string = "Hello world!";
     string no_duplicates = remove_duplicates(my_string);
@@ -82,7 +89,7 @@ void aufgabe5() {
 string remove_duplicates(string s) {
     string result = "";
 
-    for (int i = 0; i < result.length(); i++) {
+    for (int i = 0; i < s.length(); i++) {
         bool duplicate = false;
 
         for (int j = 0; j < result.length(); j++) {
@@ -103,7 +110,7 @@ string remove_duplicates(string s) {
 
 // Das Programm soll zurückgeben ob 9 eine Primzahl ist oder nicht.
 void aufgabe6() {
-    int num = 9;
+    int num = 7;
     bool is_num_prime = is_prime(num);
 
     if (is_num_prime) {
